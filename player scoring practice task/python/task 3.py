c0ebfdd5dde451219c68f89b33f6c5af9bea2afd
Dhivyesh K <63943490+Dhivyno@users.noreply.github.com>
2022-09-28 21:29:17 +0800

    Create task 3.py

---
+++ player scoring practice task/python/task 3.py
@@ -0,0 +1,34 @@
+names = []
+shots = []
+assists = []
+goals = []
+successlist = []
+total = 0
+ 
+for i in range(30):
+ name = input("Input a name:  ")
+ names.append(name)
+ shotnum = int(input("Enter the number of the player's shots:  "))
+ while shotnum > 100:
+   shotnum = int(input("The maximum number of shots is 100. Enter a valid number:  "))
+ shots.append(shotnum)
+ assistnum = int(input("Enter the number of the player's assists:  "))
+ while assistnum > 50:
+   assistnum = int(input("The maximum number of assists is 50. Enter a valid number:  "))
+ assists.append(assistnum)
+ goalnum = int(input("Enter the number of the player's goals:  "))
+ while goalnum > 40:
+   goalnum = int(input("The maximum number of goals is 40. Enter a valid number:  "))
+ goals.append(goalnum)
+ success = assistnum + 2*shotnum + 3*goalnum
+ successlist.append(success)
+for i in range(30):
+ total += successlist[i]
+ average = total/30
+highest = 0
+for i in range(30):
+ if successlist[i] > highest:
+   highest = successlist[i]
+   index = i
+print("The player with the highest success has", highest, "points and their name is", names[index], "with", shots[index], "shots,", assists[index], "assists and", goals[index], "goals")
+print("Team average is", average, "points")
